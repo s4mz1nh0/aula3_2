ff672d7655cfe73f4642a51a03d14db20def486d
Luciano Souza <luciano.souza@ifnmg.edu.br>
2019-08-08 14:25:52 -0300

    Commit inicial

--- src/aula1/Main.java
+++ src/aula1/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula1;
 
 import javafx.application.Application;
@@ -11,10 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author lusst
- */
 public class Main extends Application {
     
     @Override
